9a81cb37aee51182a13e2cc2d69f376d5f1a28cd
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-16 17:06:58 +0200

    .

--- csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -9,6 +9,6 @@ class Program
                         string middle_word = word.Substring(1, word.Length - 2);
                         Console.WriteLine("First 3 letters: {0}", word_first_3);
                         Console.WriteLine("Last 2 letters: {0}", word_last_2);
-                        Console.WriteLine("Middle word:{0}", middle_word);
+                        Console.WriteLine("Middle word: {0}", middle_word);
         }
 }
