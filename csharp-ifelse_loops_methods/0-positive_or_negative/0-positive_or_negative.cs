a0ba37f5687924dbc5d369822c572f55b9012935
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-17 15:13:23 +0200

    changes

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -3,8 +3,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random rdm = new Random();
-        int number = rdm.Next(-10, 10);
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
         if(number > 0){
             Console.WriteLine($"{number} is positive");
         }else if(number < 0){
