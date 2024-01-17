59e4c3f7f37a2cc61affe2ab8799e79b04b98c64
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-17 16:00:25 +0200

    7-islower

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,14 @@
+﻿public class Character
+{
+    public static bool IsLower(char c)
+    {
+        return c >= 'a' && c <= 'z';
+    }
+
+    static void Main()
+    {
+        char testChar = 'g';
+        bool result = IsLower(testChar);
+        Console.WriteLine($"Is '{testChar}' lowercase? {result}");
+    }
+}
