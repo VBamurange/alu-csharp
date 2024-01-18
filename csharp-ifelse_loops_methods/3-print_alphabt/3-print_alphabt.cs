e8f64e38df534802d0f6cf7c863afb871bacd0fd
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-18 15:30:44 +0200

    3-print

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+            if (letter != 'q' && letter != 'e')
+            {
+                Console.Write(letter);
+            }
+        }
+    }
+}
