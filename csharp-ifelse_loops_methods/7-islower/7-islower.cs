e90e8ec2830c896bbd9b675d634e919054c6f1d3
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-18 11:30:11 +0200

    7-islower

--- csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 
-class Character{
-
-    static List<char> lowerCase = new List<char>();
-    public static bool IsLower(char c){
-
-        for(char i = 'a'; i <= 'z'; i++){
-            lowerCase.Add(i);
-        }
-
-        if(lowerCase.Contains(c)){
-            return true;
-        }else{
-            return false;
-        }
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        // ASCII values of lowercase letters range from 97 ('a') to 122 ('z')
+        int asciiValue = (int)c;
+        
+        // Check if the ASCII value falls within the range of lowercase letters
+        return asciiValue >= 97 && asciiValue <= 122;
     }
 }
+
+class Program
+{
+    static void Main()
+    {
+        // Testing the IsLower method
+        char testChar = 'b';
+        bool isLower = Character.IsLower(testChar);
+
+        Console.WriteLine($"Is '{testChar}' a lowercase letter? {isLower}");
+    }
+};
