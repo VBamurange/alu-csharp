6f56fe5403ef7ee7a2327a5b5ceae7f3146f9aa6
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-24 15:58:37 +0200

    print list of integers

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+class List{
+
+    public static List<int> CreatePrint(int size){
+
+        if(size < 0){
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }else{
+            List<int> theeList = new List<int>();
+            for(int i = 0; i < size; i ++){
+                Console.Write(i + " ");
+                theeList.Add(i);
+            }
+            Console.WriteLine();
+            return theeList;
+        }
+    }
+}
