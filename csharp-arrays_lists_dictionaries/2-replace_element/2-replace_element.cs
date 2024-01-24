77ae13b27b6dd8c6459d9c614e5035971439eca1
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-24 13:05:52 +0200

    repace element

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+class Array{
+    public static int[] ReplaceElement(int[] array, int index, int n){
+        int[] theeArray = array;
+        int size = theeArrayArray.Length;
+        if(index >= 0 && index < size){
+            theeArray[index] = n;
+            return theeArray;
+        }else{
+            Console.WriteLine("Index out of range");
+            return theeArray;
+        }
+    }
+}
