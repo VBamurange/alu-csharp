2cefc03fff13c39db412b077fe34ef901f8620b4
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-24 17:37:10 +0200

    sorting

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+class Dictionary{
+
+    public static void PrintSorted(Dictionary<string, string> myDict){
+        var theeKeys = myDict.Keys.OrderBy(theeKeys => key).ToList();
+
+        foreach (var key in theeKeys)
+        {
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+        }
+    }
+}
