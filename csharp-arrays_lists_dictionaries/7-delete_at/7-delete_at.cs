e87ff4643663b43095f20e34a0c1dbb4a19f2b64
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-24 16:53:01 +0200

    delete at

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections;
 
-public class List{
+class List{
+
     public static List<int> DeleteAt(List<int> myList, int index){
         int ListSize = myList.Count;
         List<int> theeList = new List<int>();
 
-        if(index >= 0 && index < ListSize; i++){
+        if(index >= 0 && index < ListSize){
             for(int i = 0; i < ListSize; i++){
                 if(i == index){
-                    
+                   
                     continue;
                 }else{
-                    theeList.Add(myList[i]);
+                       
+                        theeList.Add(myList[i]);
                 }
+            
             }
+           
+
             return theeList;
         }else{
             Console.WriteLine("Index is out of range");
             return myList;
         }
+
     }
 }
