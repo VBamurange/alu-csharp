41fdc1226ccfc55ed8763ba075243abc52ade0d9
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-24 13:08:23 +0200

    replace

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -3,7 +3,7 @@
 class Array{
     public static int[] ReplaceElement(int[] array, int index, int n){
         int[] theeArray = array;
-        int size = theeArrayArray.Length;
+        int size = theeArray.Length;
         if(index >= 0 && index < size){
             theeArray[index] = n;
             return theeArray;
