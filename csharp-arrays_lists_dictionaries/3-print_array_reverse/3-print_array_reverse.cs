400c76cc7c97990d567d54c473ad2e14e2d34361
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-24 13:23:51 +0200

    print reverse

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,24 +1,24 @@
-﻿using System.Colections;
+﻿using System.Collections;
 
 class Array{
 
     public static void Reverse(int[] array){
-
-        if(array != null){
+     
+       if(array != null){
             int size = array.Length;
-            if(size > 0 && size != null){
+        if(size > 0 && size != null){
 
-                for(int i = size - 1; i >= 0; i--){
-                    Console.Write(array[i] + " ");
-                    }
-                        Console.WriteLine();
-                 }else{
+            for(int i = size - 1; i >= 0; i--){
+                Console.Write(array[i] + " ");
+                }
                     Console.WriteLine();
+             }else{
+                Console.WriteLine();
              }
-        }else{
-            Console.WriteLine();
-        }
-
-        
+       }else{
+        Console.WriteLine();
+       }
+       
+     
     }
 }
