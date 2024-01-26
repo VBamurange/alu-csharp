e36f631416f6a2c6f48195a4bbaba61b547f07b6
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-26 12:09:15 +0200

    rectangular array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = new int[5, 5]; 
+
+        matrix[2, 2] = 1;  
+
+        // Print the array
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + (j < matrix.GetLength(1) - 1 ? " " : "\n"));
+            }
+        }
+    }
+}
