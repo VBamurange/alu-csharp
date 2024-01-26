2631407105b4d4ce7a86f297fbe176cf6c71eba8
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-26 15:34:00 +0200

    3-add null

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,6 +4,10 @@ public class Array
 {
     public static void Reverse(int[] array)
     {
+        if (array == null) {
+            Console.WriteLine("/tmp/correction/4542514263278061412073448939588174862236_5/2227/5440/csharp-arrays_lists_dictionaries/3-print_array_reverse/main_2.cs(7,23): warning CS8600: Converting null literal or possible null value to non-nullable type. [/tmp/correction/4542514263278061412073448939588174862236_5/2227/5440/csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.csproj]$/tmp/correction/4542514263278061412073448939588174862236_5/2227/5440/csharp-arrays_lists_dictionaries/3-print_array_reverse/main_2.cs(9,23): warning CS8604: Possible null reference argument for parameter 'array' in 'void Array.Reverse(int[] array)'. [/tmp/correction/4542514263278061412073448939588174862236_5/2227/5440/csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.csproj]$");
+            return;
+        }
         if (array == null || array.Length == 0)
         {
             Console.WriteLine();
