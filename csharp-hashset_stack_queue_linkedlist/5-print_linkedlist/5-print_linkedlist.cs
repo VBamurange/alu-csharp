021971bd117a03e2d6adde152bb17924e5371cf2
Victoria Bamurange <v.bamurange@alustudent.com>
2024-01-29 12:17:25 +0200

    print linkedlist

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Systems.Collections.Generic;
+
+public class LList{
+    public static LinkedList<int> CreatePrint(int size){
+
+        LinkedList<int> linkedList = new LinkedList<int>();
+
+        if (size < 0)
+        {
+            return linkedList;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            linkedList.AddLast(i);
+            Console.WriteLine(i);
+        }
+
+        return linkedList;
+    }
+}
