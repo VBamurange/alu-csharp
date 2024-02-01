c10ac9f606a9b60b24b496a01e91fcdc619f2c8a
Victoria Bamurange <v.bamurange@alustudent.com>
2024-02-01 15:04:16 +0200

    insert

--- csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -1,29 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class LList{
+class LList
+{
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n){
-        if (myLList.Count == 0)
-        {
-            return myLList.AddFirst(n);
-        }
-
-        LinkedListNode<int> myNode = myLList.First!;
-
+        LinkedListNode<int> myNode = myLList.First;
         if (myNode.Value >= n)
-        {
             return myLList.AddFirst(n);
-        }
-
-        while (myNode.Next != null)
-        {
-            if (myNode.Next!.Value >= n)
-            {
-                return myLList.AddAfter(myNode, n);
+        while (myNode.Next != null){
+            if (myNode.Next.Value >= n){
+                myNode = myLList.AddAfter(myNode, n);
+                return myNode;
             }
             myNode = myNode.Next;
         }
-
-        return myLList.AddLast(n);
+        myNode = myLList.AddLast(n);
+        return myNode;
     }
 }
