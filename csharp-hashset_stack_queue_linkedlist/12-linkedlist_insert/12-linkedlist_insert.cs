81ffd84047308d67910248876a05e1b53f54ed31
Victoria Bamurange <v.bamurange@alustudent.com>
2024-02-01 14:27:58 +0200

    insert

--- csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -4,9 +4,13 @@ using System.Collections.Generic;
 class LList{
 
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n){
+        if (myLList.Count == 0){
+            return myLList.AddFirst(n);
+        }
             LinkedListNode<int> myNode = myLList.First;
-            if (myNode.Value >= n)
+            if (myNode.Value >= n){
              return myLList.AddFirst(n);
+            }
             while (myNode.Next != null){
                 if (myNode.Next.Value >= n){
                     myNode = myLList.AddAfter(myNode, n);
