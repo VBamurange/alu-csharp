fb603c853ff27207fe4fcc402f1d36ad6dbd0acf
Victoria Bamurange <v.bamurange@alustudent.com>
2024-02-20 18:41:02 +0200

    1-instance

--- csharp-inheritance/1-instance/1-instance.cs
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -1,9 +1,34 @@
 ﻿using System;
 
+/// <summary>
+/// Represents a class with a method to check if an object is an array.
+/// </summary>
 public class Obj
 {
+    /// <summary>
+    /// Checks if the specified object is an array.
+    /// </summary>
+    /// <param name="obj">The object to be checked.</param>
+    /// <returns>True if the object is an array, otherwise false.</returns>
     public static bool IsInstanceOfArray(object obj)
     {
         return obj.GetType().IsArray;
+    }
+
+    /// <summary>
+    /// Main method to demonstrate the usage of IsInstanceOfArray.
+    /// </summary>
+    public static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        if (IsInstanceOfArray(array))
+        {
+            Console.WriteLine("a is an instance of type array or inherits from type array");
+        }
+        else
+        {
+            Console.WriteLine("a is not an instance of type array or does not inherit from type array");
+        }
     }
 }
