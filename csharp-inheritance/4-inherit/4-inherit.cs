e94fc992a0815bab809a8e3183145b7eb04d27c2
Victoria Bamurange <v.bamurange@alustudent.com>
2024-02-22 18:19:01 +0200

    4-inherit

--- csharp-inheritance/4-inherit/4-inherit.cs
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -15,26 +15,3 @@ public class Dog : Animal
 {
     // Empty class
 }
-
-class Program
-{
-    static void Main()
-    {
-        // Creating instances of Animal and Dog
-        Animal genericAnimal = new Animal();
-        Dog myDog = new Dog();
-
-        // Displaying the types of the created objects
-        Console.WriteLine("Type of genericAnimal: " + genericAnimal.GetType().Name);
-        Console.WriteLine("Type of myDog: " + myDog.GetType().Name);
-
-        // Checking the relationship between Dog and Animal using IsAssignableFrom
-        bool isDogDerivedFromAnimal = typeof(Animal).IsAssignableFrom(typeof(Dog));
-        Console.WriteLine("Is Dog derived from Animal? " + isDogDerivedFromAnimal);
-
-        // Checking the relationship between genericAnimal and Dog using IsInstanceOfType
-        bool isGenericAnimalOfTypeDog = typeof(Dog).IsInstanceOfType(genericAnimal);
-        Console.WriteLine("Is genericAnimal of type Dog? " + isGenericAnimalOfTypeDog);
-    }
-}
-
