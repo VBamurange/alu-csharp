aa8af22f9a3bab23489f1e268d630586a6433e13
Victoria Bamurange <v.bamurange@alustudent.com>
2024-06-26 13:54:04 +0200

    commit

---
+++ csharp-generics/3-peek/3-main.cs
@@ -0,0 +1,42 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> myStrQ = new Queue<string>();
+
+        myStrQ.Enqueue("Hello");
+        Console.WriteLine("Number of nodes in queue: " + myStrQ.Count());
+
+        myStrQ.Enqueue("World");
+        Console.WriteLine("Number of nodes in queue: " + myStrQ.Count());
+
+        Console.WriteLine("Peek: " + myStrQ.Peek());
+        Console.WriteLine("Number of nodes in queue: " + myStrQ.Count());
+
+        Console.WriteLine("Dequeue: " + myStrQ.Dequeue());
+        Console.WriteLine("Number of nodes in queue: " + myStrQ.Count());
+
+        Console.WriteLine("Dequeue: " + myStrQ.Dequeue());
+        Console.WriteLine("Number of nodes in queue: " + myStrQ.Count());
+
+        Console.WriteLine("Dequeue: " + myStrQ.Dequeue());
+
+        Console.WriteLine("----------");
+
+        Queue<float> myFloatQ = new Queue<float>();
+
+        myFloatQ.Enqueue(4.2f);
+        myFloatQ.Enqueue(4.2f);
+        myFloatQ.Enqueue(4.2f);
+        Console.WriteLine("Number of nodes in queue: " + myFloatQ.Count());
+
+        myFloatQ.Enqueue(-9.8f);
+        Console.WriteLine("Number of nodes in queue: " + myFloatQ.Count());
+
+        Console.WriteLine("Peek: " + myFloatQ.Peek());
+        Console.WriteLine("Number of nodes in queue: " + myFloatQ.Count());
+
+        Console.WriteLine("Dequeue: " + myFloatQ.Dequeue());
+        Console.WriteLine("Number of nodes in queue: " + myFloatQ.Count());
+    }
+}
