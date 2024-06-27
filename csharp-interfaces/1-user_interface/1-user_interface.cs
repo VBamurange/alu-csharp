0be1641b593599b94c998be52ef33d948236ea24
Victoria Bamurange <v.bamurange@alustudent.com>
2024-06-27 11:22:10 +0200

    commit

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,97 @@
+﻿using System;
+/// <summary>
+/// abstract class Base
+/// </summary>
+public abstract class Base {
+/// <summary>
+/// name property
+/// </summary>
+    public string? name;
+/// <summary>
+/// ToString() method
+/// </summary>
+/// <returns></returns>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
+
+/// <summary>
+/// IInteractive interface
+/// </summary>
+public interface IInteractive {
+
+/// <summary>
+/// interact method
+/// </summary>
+    public void Interact();
+}
+
+/// <summary>
+/// IBreakable interface
+/// </summary>
+public interface IBreakable {
+
+/// <summary>
+/// durability
+/// </summary>
+    public int durability { get ; set;}
+/// <summary>
+/// break method
+/// </summary>
+    public void Break();
+}
+
+/// <summary>
+/// ICollectable interface
+/// </summary>
+public interface ICollectable{
+/// <summary>
+/// isCollected
+/// </summary>
+    public bool isCollected { get ; set; }
+
+/// <summary>
+/// collect method
+/// </summary>
+    public void Collect();
+
+}
+
+/// <summary>
+/// testObject class that inherits from base
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable {
+
+/// <summary>
+/// durability implementation
+/// </summary>
+    public int durability { get; set; }
+
+/// <summary>
+/// isCollected implementation
+/// </summary>
+    public bool isCollected { get; set; }
+
+/// <summary>
+/// interact method
+/// </summary>
+    public void Interact() {
+
+    }
+
+/// <summary>
+/// break method
+/// </summary>
+    public void Break() {
+
+    }
+
+/// <summary>
+/// collect method
+/// </summary>
+    public void Collect() {
+
+    }
+}
