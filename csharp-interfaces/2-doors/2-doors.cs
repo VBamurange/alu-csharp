8de11541f1b98714e488eae2aa5597537208ea70
Victoria Bamurange <v.bamurange@alustudent.com>
2024-06-27 12:15:17 +0200

    commit

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,81 @@
+﻿using System;
+/// <summary>
+/// abstract class Base
+/// </summary>
+public abstract class Base {
+/// <summary>
+/// name property
+/// </summary>
+    public string? name { get ; set; }
+/// <summary>
+/// ToString() method
+/// </summary>
+/// <returns></returns>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
+
+/// <summary>
+/// IInteractive interface
+/// </summary>
+public interface IInteractive {
+
+/// <summary>
+/// interact method
+/// </summary>
+    public void Interact();
+}
+
+/// <summary>
+/// IBreakable interface
+/// </summary>
+public interface IBreakable {
+
+/// <summary>
+/// durability
+/// </summary>
+    public int durability { get ; set;}
+/// <summary>
+/// break method
+/// </summary>
+    public void Break();
+}
+
+/// <summary>
+/// ICollectable interface
+/// </summary>
+public interface ICollectable{
+/// <summary>
+/// isCollected
+/// </summary>
+    public bool isCollected { get ; set; }
+
+/// <summary>
+/// collect method
+/// </summary>
+    public void Collect();
+
+}
+
+/// <summary>
+/// class Door
+/// </summary>
+public class Door : Base , IInteractive{
+
+/// <summary>
+/// constructor that sets the value of name
+/// </summary>
+/// <param name="value"></param>
+    public Door(string value = "Door"){
+        name = value;
+    }
+
+/// <summary>
+/// Interact() implementation
+/// </summary>
+    public void Interact(){
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
---
+++ csharp-interfaces/2-doors/2-main.cs
@@ -0,0 +1,11 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        Door frontDoor = new Door("Front Door");
+
+        Console.WriteLine(frontDoor.ToString());
+
+        frontDoor.Interact();
+    }
+}
