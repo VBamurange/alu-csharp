a4c7c99d608b08248d3b852bc9b77dae2216b69b
Victoria Bamurange <v.bamurange@alustudent.com>
2024-06-27 10:19:08 +0200

    commit

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+/// <summary>
+/// abstract class Base
+/// </summary>
+public abstract class Base {
+/// <summary>
+/// name property
+/// </summary>
+    public string? name;
+/// <summary>
+/// ToString() method
+/// </summary>
+/// <returns></returns>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
